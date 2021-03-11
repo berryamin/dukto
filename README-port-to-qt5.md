a25708e06a49f8074f430c9e7ee194c8a3771a9b
Olivier Kaloudoff <kaloudov@yandex.com>
2021-03-11 15:17:07 +0100

    start port for QT5

---
+++ README-port-to-qt5.md
@@ -0,0 +1,5 @@
+https://wiki.qt.io/Transition_from_Qt_4.x_to_Qt5
+
+include <QtGui>
+to
+include <QtWidgets>
--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
-#include <QtGui/QApplication>
+#include <QtWidgets/QApplication>
 #include "qmlapplicationviewer.h"
 
 #include "guibehind.h"
